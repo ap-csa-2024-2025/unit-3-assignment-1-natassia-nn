5130df9d1d2f426ffa72e760c7ce9bcf35eb8ae6
natassia-nn <natassianguyen@gmail.com>
2024-10-23 10:18:24 -0700

    made changes

--- Main.java
+++ Main.java
@@ -4,6 +4,12 @@ public class Main
 {
   public static void main(String[] args)
   {
+    Rectangle r1 = new rectangle(2, 4);
+    Rectangle r2 = new rectangle(4, 6);
+    boolean isRotated = (r1.getLength() = r2.getWidth()) && (r2.getLength() = r1.getWidth());
+    boolean isCongruent = ((r1.equals(r2)) || isRotated);
+    boolean isSimilar = (isCongruent || (r1.getLength() / r2.getLength()) == (r1.getWidth() / r2.getWidth()))
+
     // write solutions to problems here
   }
 }
